797b6178b302c658615971048242305315a4dc6c
Paul Sonkoly <sonkoly.pal@gmail.com>
2014-07-30 08:07:00 +0100

    Added STS score to puzzle report.

--- Test/Puzzle/Main.hs
+++ Test/Puzzle/Main.hs
@@ -5,7 +5,10 @@ import           Control.Applicative ((<$>))
 import           Control.Concurrent.STM (newTVarIO)
 import           Control.Monad (forM_, join, liftM)
 import qualified Data.Foldable as F
+import           Data.IORef (modifyIORef, newIORef, readIORef)
+import           Data.List (find)
 import           Data.Monoid ((<>))
+import           Numeric (showHex)
 import           System.Environment (getArgs)
 
 import           Control.Lens ((.~), (^.))
@@ -23,7 +26,6 @@ import qualified Chess.Search as S
 import           Chess.TimeControl
 import           Test.Puzzle.EPD
 
-
 ------------------------------------------------------------------------------
 data EPDResult = EPDResult
                  { ident    :: String
@@ -32,41 +34,48 @@ data EPDResult = EPDResult
                  , bestMove :: [ Move ]
                  , board    :: Maybe Board
                  , eval     :: Maybe Int
-                 , success  :: Bool
+                 , stsScore :: Int
                  }
 
 
 startResult :: EPDResult
-startResult = EPDResult "" "" Nothing [] Nothing Nothing False
+startResult = EPDResult "" "" Nothing [] Nothing Nothing 0
 
 
 ------------------------------------------------------------------------------
 main :: IO ()
 main = do
   args <- getArgs
+  scr  <- newIORef 0 
   forM_ args $ \fileName -> do
     res <- parseFromFile epdParser fileName
     case res of
       Left err   -> print err
       Right epds -> do
         putStrLn $ "Running " ++ fileName
-        eres <- runWithReport fileName epds 
-        writeFile (fileName ++ ".html") $ renderHtml eres
+        eres <- runWithReport epds
+        let tsc = sum $ map stsScore eres
+        putStrLn $ "score : " ++ show tsc
+        modifyIORef scr (+tsc)
+        writeFile (fileName ++ ".html") $
+          renderHtml $ Main.report fileName $ eres
+  tsc <- readIORef scr
+  putStrLn $ "Total score : " ++ show tsc
 
 
 ------------------------------------------------------------------------------
-runWithReport :: String -> [ EPD ] -> IO H.Html
-runWithReport fn epds = liftM (Main.report fn) (mapM runEPD epds `using` rpar)
+runWithReport :: [EPD] -> IO [EPDResult]
+runWithReport epds = mapM runEPD epds `using` rpar
 
 
 ------------------------------------------------------------------------------
 runEPD :: EPD -> IO EPDResult
-runEPD (EPD b ops) = F.foldrM (applyOp b) startResult ops
+runEPD (EPD b ops) = F.foldlM (applyOp b) startResult ops
 
 
 ------------------------------------------------------------------------------
-applyOp :: Board -> Operation -> EPDResult -> IO EPDResult
-applyOp b (BestMove ms) epdr = do
+applyOp :: Board -> EPDResult -> Operation -> IO EPDResult
+applyOp b epdr (BestMove ms) = do
   r <- silence $ do
     abortVal  <- newTVarIO False
     ponderVal <- newTVarIO False
@@ -75,18 +84,22 @@ applyOp b (BestMove ms) epdr = do
   let m = join $ S.first <$> r
   return $ epdr
                { ourMove  = m
-               , success  = m `elem` map Just ms
                , bestMove = ms
                , eval     = (^.S.eval) <$> r
                }
   
-applyOp b (Id ids) epdr = do
+applyOp b epdr (Id ids) = do
   putStrLn $ "  Running " ++ ids
   return $ epdr { board = Just b, ident = ids }
   
-applyOp _ (Comment _ cs) epdr = return $ epdr { comment = show $ cs }
+applyOp _ epdr (Comment _ (Left cs)) = return $ epdr { comment = cs }
+applyOp _ epdr (Comment _ (Right l)) =
+  let score = case find ((== ourMove epdr) . Just . fst) l of
+        Just (_, s) -> s
+        Nothing     -> 0
+  in return $ epdr { stsScore = score }
 
-applyOp _ _ epdr = return epdr
+applyOp _ epdr _  = return epdr
 
 
 ------------------------------------------------------------------------------
@@ -102,23 +115,31 @@ report fn epdrs =
             H.td "Identifier"
             H.td "FEN"
             H.td "Our move"
+            H.td "STS score"
             H.td "Best move(s)"
-            H.td "Our evaluation"
+            H.td "Our evaluation"            
             H.td "Comment"
         H.tbody $ F.foldr1 (<>) $ map reportEPD epdrs
 
 
 ------------------------------------------------------------------------------
 reportEPD :: EPDResult -> H.Html
-reportEPD (EPDResult i c mm bm mb me s) =
-  H.tr ! A.style (preEscapedToValue $ scolour s) $ do
+reportEPD (EPDResult i c mm bm mb me sc) =
+  H.tr ! A.style (preEscapedToValue $ scoreCSS sc) $ do
     H.td $ H.toHtml i
     H.td $ H.toHtml $ maybe "No board" fen mb
     H.td $ H.toHtml $ maybe "No move" renderShortMove mm
+    H.td $ H.toHtml sc
     H.td $ H.toHtml $ show $ map renderShortMove bm
     H.td $ H.toHtml $ maybe "No evaluation" show me
     H.td $ H.toHtml c
-  where
-    scolour :: Bool -> String
-    scolour True  = "background-color:green;"
-    scolour False = "background-color:red;"
+
+
+scoreCSS :: Int -> String
+scoreCSS sc =
+  let a = 25 * sc
+      b = max 16 $ a - 50
+      s = case sc of
+        0 -> "red"
+        _ -> '#' : (showHex b . showHex a . showHex b $ "")
+  in "background-color:" ++ s ++";"
